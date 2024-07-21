09effdfb078f4349c3e209a025a41ed9f761e417
Thushara Thiwanka <thusharathiwanka01@gmail.com>
2024-07-21 20:11:34 +0530

    feat: Add JokeTypesModule to app module imports and configure TypeORM for MySQL database

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,28 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { JokesModule } from './jokes/jokes.module';
+import { JokeTypesModule } from './joke-types/joke-types.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Joke } from './jokes/entities/Joke';
+import { JokeType } from './joke-types/entities/JokeType';
 
 @Module({
-  imports: [JokesModule],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.MYSQL_DB_HOST,
+      port: parseInt(process.env.MYSQL_DB_PORT),
+      username: process.env.MYSQL_USER,
+      password: process.env.MYSQL_PASSWORD,
+      database: process.env.MYSQL_DATABASE,
+      entities: [Joke, JokeType],
+      synchronize: true, // ! only for development
+    }),
+    JokesModule,
+    JokeTypesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
