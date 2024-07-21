768d5175d6f13fc80019ba3feb37ecd5a3693d27
Thushara Thiwanka <thusharathiwanka01@gmail.com>
2024-07-21 20:11:25 +0530

    chore: Enable CORS and add Swagger documentation to the API

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: [process.env.LOCAL_DOMAIN, process.env.PROD_DOMAIN],
+  });
+
+  const config = new DocumentBuilder()
+    .setTitle('Deliver Jokes Microservice')
+    .setDescription('The Deliver Jokes API documentation')
+    .setVersion('1.0')
+    .addTag('jokes')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3003);
 }
 bootstrap();
