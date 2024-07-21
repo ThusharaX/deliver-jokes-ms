2027a42958e59f2d8ead02d4906f03dc11523a96
Thushara Thiwanka <thusharathiwanka01@gmail.com>
2024-07-21 20:12:26 +0530

    feat: Add unit tests for joke types and jokes controllers and services

---
+++ src/joke-types/controllers/joke-types.controller.spec.ts
@@ -0,0 +1,18 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { JokeTypesController } from './joke-types/joke-types.controller';
+
+describe('JokeTypesController', () => {
+  let controller: JokeTypesController;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [JokeTypesController],
+    }).compile();
+
+    controller = module.get<JokeTypesController>(JokeTypesController);
+  });
+
+  it('should be defined', () => {
+    expect(controller).toBeDefined();
+  });
+});
